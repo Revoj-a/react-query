20a6dfe892b664b454129fad266fbf41efd723ed
Revoj-a <revojjoever@gmail.com>
2025-12-18 21:50:59 +0800

    Showing a loading indicator

--- react-query/src/components/TodoList.tsx
+++ react-query/src/components/TodoList.tsx
@@ -13,17 +13,25 @@ const TodoList = () => {
     axios
       .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
       .then((res) => res.data);
-  const { data: todos, error } = useQuery<Todo[], Error>({
+  const {
+    data: todos,
+    error,
+    isLoading,
+  } = useQuery<Todo[], Error>({
     queryKey: ["todos"],
     queryFn: fetchTodos,
   });
+
+  if (isLoading) return <p>Loading...</p>;
 
   if (error) return <p>{error.message}</p>;
 
   return (
     <ul className="list-group">
       {todos?.map((todo) => (
-        <li className="list-group-item">{todo.title}</li>
+        <li key={todo.id} className="list-group-item">
+          {todo.title}
+        </li>
       ))}
     </ul>
   );
