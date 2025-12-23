64fb481f51a7e1f6e16e4b4fa5b6a19bf14af543
Revoj-a <revojjoever@gmail.com>
2025-12-23 19:42:22 +0800

    Create counter component

--- react-query/src/App.tsx
+++ react-query/src/App.tsx
@@ -2,12 +2,12 @@ import "./App.css";
 import PostList from "./components/PostList";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
+import Counter from "./state-management/Counter";
 
 function App() {
   return (
     <>
-      <TodoForm />
-      <TodoList />
+      <Counter />
     </>
   );
 }
