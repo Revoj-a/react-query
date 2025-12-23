6406de1d2ea49e65829638cd18c5d66e159e9394
Revoj-a <revojjoever@gmail.com>
2025-12-23 23:08:47 +0800

    Working on Reducer

---
+++ react-query/src/state-management/LoginStatus.tsx
@@ -0,0 +1,29 @@
+import { useReducer } from "react";
+import loginStatusReducer from "./reducers/loginStatusReducer";
+
+const LoginStatus = () => {
+  const [user, dispatch] = useReducer(loginStatusReducer, "");
+  if (user)
+    return (
+      <>
+        <div>
+          <span className="mx-2">{user}</span>
+          <a onClick={() => dispatch({ type: "LOGOUT" })} href="#">
+            Logout
+          </a>
+        </div>
+      </>
+    );
+  return (
+    <div>
+      <a
+        onClick={() => dispatch({ type: "LOGIN", username: "mosh.hademani" })}
+        href="#"
+      >
+        Login
+      </a>
+    </div>
+  );
+};
+
+export default LoginStatus;
