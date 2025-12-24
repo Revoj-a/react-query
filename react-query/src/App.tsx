9a000832001e146213091b8a358500dd9bc163ee
Revoj-a <revojjoever@gmail.com>
2025-12-24 18:37:50 +0800

    Implementing custom provider

--- react-query/src/App.tsx
+++ react-query/src/App.tsx
@@ -2,22 +2,21 @@ import { useReducer } from "react";
 import "./App.css";
 import taskReducer from "./state-management/reducers/taskReducer";
 
+import AuthProvider from "./state-management/AuthProvider";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
 import TasksContext from "./state-management/contexts/tasksContext";
-import loginStatusReducer from "./state-management/reducers/loginStatusReducer";
-import AuthActionContext from "./state-management/contexts/loginStatusContext";
 
 function App() {
   const [tasks, tasksDispatch] = useReducer(taskReducer, []);
-  const [user, authDispatch] = useReducer(loginStatusReducer, "");
+
   return (
-    <AuthActionContext.Provider value={{ user, dispatch: authDispatch }}>
+    <AuthProvider>
       <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
         <NavBar />
         <HomePage />
       </TasksContext.Provider>
-    </AuthActionContext.Provider>
+    </AuthProvider>
   );
 }
 
