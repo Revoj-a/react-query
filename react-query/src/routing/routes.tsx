228efb736beb8237bdb674f35e2c3e1fa6943c26
Revoj-a <revojjoever@gmail.com>
2025-12-26 21:23:33 +0800

    Passing data with Route Parameter

--- react-query/src/routing/routes.tsx
+++ react-query/src/routing/routes.tsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import ContactPage from "./ContactPage";
+import UserListPage from "./UserListPage";
+import UserDetailsPage from "./UserDetailsPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
+  { path: "/users", element: <UserListPage /> },
+  { path: "/users/:id", element: <UserDetailsPage /> },
   { path: "/contact", element: <ContactPage /> },
 ]);
 
